c8db35c24a60d7e8acdaeaaaf60e7f42ef3f6ac7
mchmddiazz <pomcat.14@gmail.com>
2024-12-31 04:15:38 +0700

    disabled mysql module

--- src/app/api/test-db/route.ts
+++ src/app/api/test-db/route.ts
@@ -1,28 +1,28 @@
-// app/api/test-db/route.ts
+// // app/api/test-db/route.ts
 
-import { NextResponse } from 'next/server';
-import db from '../../../lib/db'; // Import the database connection
+// import { NextResponse } from 'next/server';
+// import db from '../../../lib/db'; // Import the database connection
 
-export async function GET() {
-  try {
-    // Test the MySQL connection by executing a simple query
-    const [rows] = await db.query('SELECT 1 + 1 AS result');
+// export async function GET() {
+//   try {
+//     // Test the MySQL connection by executing a simple query
+//     const [rows] = await db.query('SELECT 1 + 1 AS result');
 
-    // If the query runs successfully, return a success response
-    return NextResponse.json({
-      success: true,
-      message: 'MySQL connection is successful',
-      result: rows[0],
-    });
-  } catch (error) {
-    // If there's an error, return a failure response with the error message
-    return NextResponse.json(
-      {
-        success: false,
-        message: 'Failed to connect to MySQL',
-        error: error.message,
-      },
-      { status: 500 }
-    );
-  }
-}
+//     // If the query runs successfully, return a success response
+//     return NextResponse.json({
+//       success: true,
+//       message: 'MySQL connection is successful',
+//       result: rows[0],
+//     });
+//   } catch (error) {
+//     // If there's an error, return a failure response with the error message
+//     return NextResponse.json(
+//       {
+//         success: false,
+//         message: 'Failed to connect to MySQL',
+//         error: error.message,
+//       },
+//       { status: 500 }
+//     );
+//   }
+// }
